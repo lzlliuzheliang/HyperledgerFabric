12ce2b6214bd53fa573fc14299dffb4efa8a121e
lzlliuzheliang <lzlliuzheliang@gmail.com>
2018-04-01 14:35:33 -0400

    added .gitignore

--- basic-network/networkUp.sh
+++ basic-network/networkUp.sh
@@ -1,13 +1,13 @@
-# CLI_TIMEOUT=10
-# # default for delay between commands
-# CLI_DELAY=3
+CLI_TIMEOUT=10
+# default for delay between commands
+CLI_DELAY=3
 # # channel name defaults to "mychannel"
-# CHANNEL_NAME="mychannel"
-# # use this as the default docker-compose yaml definition
+CHANNEL_NAME="mychannel"
+# use this as the default docker-compose yaml definition
 # COMPOSE_FILE=docker-compose.yaml
 # # use golang as the default language for chaincode
-# LANGUAGE=golang
-# # default image tag
+LANGUAGE=golang
+# default image tag
 # IMAGETAG="latest"
 # TIMEOUT=300
 #CHANNEL_NAME=mychannel TIMEOUT=60 docker-compose -f docker-compose.yml up -d
